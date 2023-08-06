676ecda3a6a307399812d27ff0829a3ca1543f1e
xDannyx <danix7123@gmail.com>
2023-08-06 15:48:04 +0200

    connection with vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>xDannyx's app</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
